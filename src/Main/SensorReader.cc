9fa102a2c604807711ce7b042c217ea399eb95af
Imad Azzam <imad.azzam@outlook.com>
2017-10-14 19:27:17 -0400

    Initial styling and fixes in Main/SensorReader files

--- src/Main/SensorReader.cc
+++ src/Main/SensorReader.cc
@@ -4,24 +4,38 @@
 #include "SensorReader.h"
 #include "Config.h"
 
-long lastTime = millis();
+// Global variables are not recommended unless absolutely recommended.
+// Avoiding global variables will reduce name conflicts in the future.
+// This can be moved to the Read function and made static, or simply
+// made another member variable.
+long glLastTime = millis(); // g means global, l means long
 
-SensorReader::SensorReader(State &state) : state(state){
-	flmRpm = new Rpm{"rpmflm", 260, config.flmSensor};
-	frmRpm = new Rpm{"rpmfrm", 260, config.frmSensor};
+SensorReader::SensorReader(State &rState) :
+	m_rState(rState), // Long initialization lists are easier to read this way
+	m_pFlmRpm(nullptr),
+	m_pFrmRpm(nullptr),
+	m_pRlmRpm(nullptr),
+	m_pRrmRpm(nullptr)
+{
+	// A value of 260 is over the max of uint8_t (that is 255).
+	// Fix warning by using the max defined by UINT8_MAX.
+	m_pFlmRpm = new Rpm("rpmflm", UINT8_MAX, config.flmSensor); // No good reason for list initialization
+	m_pFrmRpm = new Rpm("rpmfrm", UINT8_MAX, config.frmSensor);
 }
 
-void SensorReader::read(){
-	state.currRpmFL = flmRpm->read();
+void SensorReader::Read(){
+	m_rState.currRpmFL = m_pFlmRpm->read();
 
-	if (millis() - lastTime >= 200){
-		//wlp::serial << *flmRpm;
+	if (millis() - glLastTime >= 200){
+		//wlp::serial << *m_pFlmRpm;
+
+		// glLastTime = millis(); // Doesn't glLastTime need to be updated here?
 	}
 }
 
 SensorReader::~SensorReader(){
-	delete flmRpm;
-	delete frmRpm;
-	delete rlmRpm;
-	delete rrmRpm;
+	delete m_pFlmRpm;
+	delete m_pFrmRpm;
+	delete m_pRlmRpm;
+	delete m_pRrmRpm;
 }
--- src/Main/SensorReader.h
+++ src/Main/SensorReader.h
@@ -1,22 +1,34 @@
 #ifndef CONTROL_SENSORREADER_H
 #define CONTROL_SENSORREADER_H
 
-#include <sensors/mag/Rpm.h>
+// Header files created by us that are part of the project
+// should use quotes instead of <> (used for pre-existing
+// system headers).
+#include "sensors/mag/Rpm.h"
 
 class State;
 
 class SensorReader{
-	State &state;
-	Rpm *flmRpm;
-	Rpm *frmRpm;
-	Rpm *rrmRpm = nullptr;
-	Rpm *rlmRpm = nullptr;
+
 public:
-	SensorReader(State &state);
-
+	SensorReader(State &rState);
+	
 	~SensorReader();
+	
+	void Read(); // public functions should be capitalized
 
-	void read();
+private: // Access keywords should always be declared regardless of default
+
+	// It is  uncommon to have reference member variables, is there a reason this
+	// is not a pointer instead?
+	State &m_rState; // m_ indicates a member variable, r indicates this is a reference
+
+	// Do you know if any library for Arduino supports smart pointers?
+	Rpm *m_pFlmRpm; // p indicates this is a pointer
+	Rpm *m_pFrmRpm;
+	Rpm *m_pRlmRpm; // Initialzation should happen in constructor.
+	Rpm *m_pRrmRpm;
+
 };
 
 #endif //CONTROL_SENSORREADER_H
